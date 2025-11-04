5a9ccb97b877cc1fc2880e0c9e58c2fd15f70c3d
MASAMICHI IIZUMI <m.iizumi@miosync.email>
2025-11-04 18:53:31 +0900

    Delete tests directory

--- tests/test_smoke.py
+++
@@ -1,7 +0,0 @@
-from lambda3_holo.automaton import Automaton
-
-def test_smoke_small_run():
-    ua = Automaton(H=16, W=16, Z=8, gate_delay=1, gate_strength=0.1, c_eff_max=0.18, seed=123)
-    ua.boundary = ua.coop_field()
-    rec = ua.step_once(1)
-    assert "entropy_RT_mo" in rec and "lambda_p99_A_out_pre" in rec
